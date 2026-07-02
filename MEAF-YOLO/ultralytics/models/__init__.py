962ec5ec52972508c7ed489887ee90cc60a3a1e9
fyw-cc <fyw2054846179@gmail.com>
2026-07-03 00:37:12 +0800

    Delete MEAF-YOLO directory

--- MEAF-YOLO/ultralytics/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
-
-from .yolo import YOLO
-
-__all__ = ("YOLO",)
--- MEAF-YOLO/ultralytics/models/yolo/__init__.py
+++
@@ -1,5 +0,0 @@
-# Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
-
-from .model import YOLO
-
-__all__ = ("YOLO",)
