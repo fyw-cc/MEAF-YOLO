96ed74167c48a37a270e2bb57c6980f8d014f7e5
fyw-cc <fyw2054846179@gmail.com>
2026-07-20 00:23:52 +0800

    Delete train.py

--- train.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-import warnings
-warnings.filterwarnings("ignore")
-
-if __name__ == '__main__':
-    model = YOLO('ultralytics/cfg/MEAF-YOLO/MEAF-YOLO.yaml', task="detect")
-
-    model.train(data='ultralytics/cfg/datasets/VisDrone.yaml',batch=8,epochs=200,imgsz=640)
